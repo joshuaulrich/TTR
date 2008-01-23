7fbef6825cfa863929f8603c48d192f29270427a
jryan <jryan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-01-23 17:04:28 +0000

    fixed trailing 0s to trailing NAs

--- pkg/R/DPO.R
+++ pkg/R/DPO.R
@@ -15,15 +15,13 @@ function(x, n=10, maType="SMA", shift=n/2+1, percent=FALSE, ...) {
 
   maArgs <- list(n=n, ...)
   mavg <- do.call( maType, c( list(x), maArgs ) )
-  mavg <- c( mavg[-c(1:shift)], rep(0, shift) )
+  mavg <- c( mavg[-c(1:shift)], rep(NA, shift) )
 
   if(percent) {
     DPO <- 100 * ( x / mavg - 1 )
   } else {
     DPO <- x - mavg
   }
-
-  DPO[(NROW(DPO)-shift+1):NROW(DPO)] <- 0
 
   return( DPO )
 }

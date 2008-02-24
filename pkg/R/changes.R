99555a3ad23002ce4c4d218ccb2ca8e36815ac9c
bodanker <bodanker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-02-24 19:02:08 +0000

    - Revert ROC function to previous version

--- pkg/R/changes.R
+++ pkg/R/changes.R
@@ -15,15 +15,15 @@ function(x, n=1, type=c("continuous","discrete"), na=NA) {
 
   x    <- as.vector(x)
   roc  <- vector("numeric", NROW(x))
-  #type <- match.arg(type) match.arg only works with 1 arg cleanly -jar
+  type <- match.arg(type)
 
   # Discrete changes
-  if(type[1]=="discrete") {
+  if(type=="discrete") {
     roc <- c( rep(na,n),      x[(1+n):NROW(x)] / x[1:(NROW(x)-n)] -1 )
   }
 
   # Continuous changes
-  if(type[1]=="continuous") {
+  if(type=="continuous") {
     roc <- c( rep(na,n), log( x[(1+n):NROW(x)] / x[1:(NROW(x)-n)] )  )
   }
 

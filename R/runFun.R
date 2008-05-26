be22d23286f63ac0fc4b6a7c7db2e0563b0b7a2e
Joshua M. Ulrich <josh.m.ulrich@gmail.com>
2008-05-26 18:24:49 +0000

    - runFuns no longer change the input variable(s), in order to use the   match.to functionality of reclass().
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/ttr/pkg@38 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/runFun.R
+++ R/runFun.R
@@ -10,32 +10,33 @@ function(x, n=10) {
 
   if( n < 1 || n > NROW(x) ) stop("Invalid 'n'")
 
-  # Count NAs, ensure they're only at beginning of data, then remove.
-  NAs <- sum( is.na(x) )
+  # Count NAs, ensure they're only at beginning of data.
+  NAs <- sum(is.na(x))
   if( NAs > 0 ) {
     if( any( is.na(x[-(1:NAs)]) ) ) stop("Series contains non-leading NAs")
-    x <- x[-(1:NAs)]
   }
+  beg <- 1 + NAs
+  len <- NROW(x) - NAs
 
   # Initialize result vector 
-  result <- rep(0,NROW(x))
-  result[n] <- sum(x[1:n])
+  result <- double(NROW(x))
+  result[(n+beg-1)] <- sum(x[beg:(n+beg-1)])
 
   # Call Fortran routine
   result <- .Fortran( "runsum",
-                   ia = as.double(x),
-                   lia = as.integer(NROW(x)),
+                   ia = as.double(x[beg:NROW(x)]),
+                   lia = as.integer(len),
                    n = as.integer(n),
-                   oa = as.double(result),
-                   loa = as.integer(NROW(result)),
+                   oa = as.double(result[beg:NROW(x)]),
+                   loa = as.integer(len),
                    PACKAGE = "TTR" )$oa
 
+  # Replace 1:(n-1) with NAs and prepend NAs from original data
+  is.na(result) <- c(1:(n-1))
+  result <- c( rep( NA, NAs ), result )
+  
   # Convert back to original class
   reclass(result, x)
-
-  # Replace 1:(n-1) with NAs and prepend NAs from original data
-  result[1:(n-1)] <- NA
-  result <- c( rep( NA, NAs ), result )
 }
 
 #-------------------------------------------------------------------------#
@@ -51,26 +52,24 @@ function(x, n=10) {
   NAs <- sum( is.na(x) )
   if( NAs > 0 ) {
     if( any( is.na(x[-(1:NAs)]) ) ) stop("Series contains non-leading NAs")
-    x <- x[-(1:NAs)]
   }
-
-  # Initialize result vector 
-  result <- x
+  beg <- 1 + NAs
+  len <- NROW(x) - NAs
 
   result <- .Fortran( "wilder",
-                   ia  = as.double(x),
-                   lia = as.integer(NROW(x)),
+                   ia  = as.double(x[beg:NROW(x)]),
+                   lia = as.integer(len),
                    n   = as.integer(n),
-                   oa  = as.double(result),
-                   loa = as.integer(NROW(result)),
+                   oa  = as.double(x[beg:NROW(x)]),
+                   loa = as.integer(len),
                    PACKAGE = "TTR" )$oa
+
+  # Replace 1:(n-1) with NAs and prepend NAs from original data
+  is.na(result) <- c(1:(n-1))
+  result <- c( rep( NA, NAs ), result )
 
   # Convert back to original class
   reclass(result, x)
-
-  # Replace 1:(n-1) with NAs and prepend NAs from original data
-  result[1:(n-1)] <- NA
-  result <- c( rep( NA, NAs ), result )
 }
 
 #-------------------------------------------------------------------------#
@@ -86,27 +85,28 @@ function(x, n=10) {
   NAs <- sum( is.na(x) )
   if( NAs > 0 ) {
     if( any( is.na(x[-(1:NAs)]) ) ) stop("Series contains non-leading NAs")
-    x <- x[-(1:NAs)]
   }
+  beg <- 1 + NAs
+  len <- NROW(x) - NAs
 
   # Initialize result vector 
-  result <- rep(0,NROW(x))
-  result[n] <- min(x[1:n])
+  result <- double(NROW(x))
+  result[(n+beg-1)] <- min(x[beg:(n+beg-1)])
 
   result <- .Fortran( "runmin",
-                   ia = as.double(x),
-                   lia = as.integer(NROW(x)),
+                   ia = as.double(x[beg:NROW(x)]),
+                   lia = as.integer(len),
                    n = as.integer(n),
-                   oa = as.double(result),
-                   loa = as.integer(NROW(result)),
+                   oa = as.double(result[beg:NROW(x)]),
+                   loa = as.integer(len),
                    PACKAGE = "TTR" )$oa
+
+  # Replace 1:(n-1) with NAs and prepend NAs from original data
+  is.na(result) <- c(1:(n-1))
+  result <- c( rep( NA, NAs ), result )
 
   # Convert back to original class
   reclass(result, x)
-
-  # Replace 1:(n-1) with NAs and prepend NAs from original data
-  result[1:(n-1)] <- NA
-  result <- c( rep( NA, NAs ), result )
 }
 
 #-------------------------------------------------------------------------#
@@ -122,27 +122,28 @@ function(x, n=10) {
   NAs <- sum( is.na(x) )
   if( NAs > 0 ) {
     if( any( is.na(x[-(1:NAs)]) ) ) stop("Series contains non-leading NAs")
-    x <- x[-(1:NAs)]
   }
+  beg <- 1 + NAs
+  len <- NROW(x) - NAs
 
   # Initialize result vector 
-  result <- rep(0,NROW(x))
-  result[n] <- max(x[1:n])
+  result <- double(NROW(x))
+  result[(n+beg-1)] <- max(x[beg:(n+beg-1)])
 
   result <- .Fortran( "runmax",
-                   ia = as.double(x),
-                   lia = as.integer(NROW(x)),
+                   ia = as.double(x[beg:NROW(x)]),
+                   lia = as.integer(len),
                    n = as.integer(n),
-                   oa = as.double(result),
-                   loa = as.integer(NROW(result)),
+                   oa = as.double(result[beg:NROW(x)]),
+                   loa = as.integer(len),
                    PACKAGE = "TTR" )$oa
+
+  # Replace 1:(n-1) with NAs and prepend NAs from original data
+  is.na(result) <- c(1:(n-1))
+  result <- c( rep( NA, NAs ), result )
 
   # Convert back to original class
   reclass(result, x)
-
-  # Replace 1:(n-1) with NAs and prepend NAs from original data
-  result[1:(n-1)] <- NA
-  result <- c( rep( NA, NAs ), result )
 }
 
 #-------------------------------------------------------------------------#
@@ -168,31 +169,29 @@ function(x, n=10, non.unique="mean") {
   NAs <- sum( is.na(x) )
   if( NAs > 0 ) {
     if( any( is.na(x[-(1:NAs)]) ) ) stop("Series contains non-leading NAs")
-    x <- x[-(1:NAs)]
   }
+  beg <- 1 + NAs
+  len <- NROW(x) - NAs
 
   # Non-unique median
   non.unique <- match.arg(non.unique, c('mean','max','min'))
   non.unique <- switch( non.unique, mean=0, max=1, min=-1 )
   
-  # Initialize result vector 
-  result <- rep(0,NROW(x))
-
   # Call Fortran routine
   result <- .Fortran( "runmedian",
-                   ia = as.double(x),
+                   ia = as.double(x[beg:NROW(x)]),
                    n = as.integer(n),
-                   oa = double(NROW(x)),
-                   la = as.integer(NROW(x)),
+                   oa = double(len),
+                   la = as.integer(len),
                    ver = as.integer(non.unique),
                    PACKAGE = "TTR" )$oa
 
+  # Replace 1:(n-1) with NAs and prepend NAs from original data
+  is.na(result) <- c(1:(n-1))
+  result <- c( rep( NA, NAs ), result )
+
   # Convert back to original class
   reclass(result, x)
-
-  # Replace 1:(n-1) with NAs and prepend NAs from original data
-  result[1:(n-1)] <- NA
-  result <- c( rep( NA, NAs ), result )
 }
 
 #-------------------------------------------------------------------------#
@@ -200,7 +199,13 @@ function(x, n=10, non.unique="mean") {
 "runCov" <-
 function(x, y, n=10, use="all.obs", sample=TRUE) {
 
-  xy <- cbind( as.vector(x), as.vector(y) )
+  x <- use.xts(x, error=FALSE)
+  y <- use.xts(y, error=FALSE)
+  if(is.xts(x) && is.xts(y)) {
+    xy <- cbind(x,y)
+  } else {
+    xy <- cbind( as.vector(x), as.vector(y) )
+  }
 
   if( n < 1 || n > NROW(x) ) stop("Invalid 'n'")
 
@@ -212,34 +217,34 @@ function(x, y, n=10, use="all.obs", sample=TRUE) {
   NAs <- max( xNAs, yNAs )
   if( NAs > 0 ) {
     if( any( is.na(xy[-(1:NAs),]) ) ) stop("Series contain non-leading NAs")
-    xy <- xy[-(1:NAs)]
   }
+  beg <- 1 + NAs
+  len <- NROW(xy) - NAs
   
   xCenter <- runSum(x, n)/n
-  xCenter[1:(n-1)] <- 0
+  xCenter[1:(NAs+n-1)] <- 0
   yCenter <- runSum(y, n)/n
-  yCenter[1:(n-1)] <- 0
-
-  # Initialize result vector 
-  result <- rep(0,NROW(xy))
+  yCenter[1:(NAs+n-1)] <- 0
 
   # Call Fortran routine
   result <- .Fortran( "runCov",
-                   rs1 = as.double(x),
-                   avg1 = as.double(xCenter),
-                   rs2 = as.double(y),
-                   avg2 = as.double(yCenter),
-                   la = as.integer(NROW(x)),
+                   rs1 = as.double(x[beg:NROW(xy)]),
+                   avg1 = as.double(xCenter[beg:NROW(xy)]),
+                   rs2 = as.double(y[beg:NROW(xy)]),
+                   avg2 = as.double(yCenter[beg:NROW(xy)]),
+                   la = as.integer(len),
                    n = as.integer(n),
                    samp = as.integer(sample),
-                   oa = as.double(result),
+                   oa = double(len),
                    PACKAGE = "TTR" )$oa
 
   # Replace 1:(n-1) with NAs and prepend NAs from original data
-  result[1:(n-1)] <- NA
+  is.na(result) <- c(1:(n-1))
   result <- c( rep( NA, NAs ), result )
 
-  return( result )
+  # Convert back to original class
+  # Should the attributes of *both* x and y be retained?
+  reclass(result, x)
 }
 
 #-------------------------------------------------------------------------#
@@ -287,12 +292,10 @@ function(x, n=10, center=runMedian(x, n), stat="median",
   NAs <- sum( is.na(x) )
   if( NAs > 0 ) {
     if( any( is.na(x[-(1:NAs)]) ) ) stop("Series contains non-leading NAs")
-    x <- x[-(1:NAs)]
   }
-  center[1:(n-1)] <- 0
-
-  # Initialize result vector 
-  result <- rep(0,NROW(x))
+  beg <- 1 + NAs
+  len <- NROW(x) - NAs
+  center[1:(NAs+n-1)] <- 0
 
   # Mean or Median absolute deviation?
   median <- match.arg(stat, c("mean","median"))
@@ -304,21 +307,21 @@ function(x, n=10, center=runMedian(x, n), stat="median",
   
   # Call Fortran routine
   result <- .Fortran( "runMAD",
-                   rs = as.double(x),
-                   cs = as.double(center),
-                   la = as.integer(NROW(x)),
-                   n = as.integer(n),
-                   oa = as.double(result),
-                   stat = as.integer(median),
-                   ver = as.integer(non.unique),
+                   rs = as.double(x[beg:NROW(x)]),      # raw series
+                   cs = as.double(center[beg:NROW(x)]), # center series
+                   la = as.integer(len),                # length of input arrays
+                   n = as.integer(n),                   # size of rolling window
+                   oa = double(len),                    # output array
+                   stat = as.integer(median),           # center statistic
+                   ver = as.integer(non.unique),        # median type
                    PACKAGE = "TTR" )$oa
 
   if( median ) result <- result * constant
 
+  # Replace 1:(n-1) with NAs and prepend NAs from original data
+  is.na(result) <- c(1:(n-1))
+  result <- c( rep( NA, NAs ), result )
+
   # Convert back to original class
   reclass(result, x)
-
-  # Replace 1:(n-1) with NAs and prepend NAs from original data
-  result[1:(n-1)] <- NA
-  result <- c( rep( NA, NAs ), result )
 }

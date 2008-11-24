4c712cdacaae226319e9ee7e80cef0c86439d66d
bodanker <bodanker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2008-11-24 03:23:30 +0000

    - Made the following functions xts-aware:   - aroon   - ATR - Changed internals of aroon(), so the same code can run on xts   and non-xts objects - aroon() calculation starts at period n+1, instead of n - Changed aroon() and ATR() output in unit test data to account   for the above change - Added NA to first element of closeLag of ATR() and changed   unit tests to account for this change

--- pkg/R/aroon.R
+++ pkg/R/aroon.R
@@ -13,7 +13,8 @@ function(HL, n=20) {
   # http://www.linnsoft.com/tour/techind/aroon.htm
   # http://stockcharts.com/education/IndicatorAnalysis/indic-Aroon.htm
 
-  HL <- as.matrix(HL)
+  HL <- try.xts(HL, error=FALSE)
+
   aroonUp <- aroonDn <- vector("numeric",NROW(HL))
 
   # Calculation if price vector is given
@@ -30,17 +31,19 @@ function(HL, n=20) {
 
   stop("Price series must be either High-Low, or Close")
 
-  # Find max and min of price series
+  # Find max and min of price series over past (n+1) days
+  # It must be (n+1) to cover today, and the past n days
   hmax <- runMax(high, n+1)
   lmin <- runMin( low, n+1)
 
   # Calculate Aroon UP and DOWN
-  for(i in n:NROW(HL)) {
-    aroonUp[i] <- 100 * ( ( n - (match(hmax[i], high[i:(i-n)])-1) ) / n )
-    aroonDn[i] <- 100 * ( ( n - (match(lmin[i],  low[i:(i-n)])-1) ) / n )
+  for(i in (n+1):NROW(HL)) {
+    aroonUp[i] <- 100 * ( max((0:n)[high[(i-n):i] %in% hmax[i]]) / n )
+    aroonDn[i] <- 100 * ( max((0:n)[ low[(i-n):i] %in% lmin[i]]) / n )
   }
 
   oscillator <- aroonUp - aroonDn
 
-  return( cbind( aroonUp, aroonDn, oscillator ) )
+  # Convert back to original class
+  reclass( cbind( aroonUp, aroonDn, oscillator ), HL )
 }
--- pkg/tests/unitTests/runit.TTR.Trend.R
+++ pkg/tests/unitTests/runit.TTR.Trend.R
@@ -11,9 +11,13 @@ data(ttrc)
 rownames(ttrc) <- ttrc$Date
 ttrc$Date <- NULL
 
-input <- list( all=ttrc[1:250,], top=ttrc[1:250,], mid=ttrc[1:250,] )
-input$top[1:10,] <- NA
-input$mid[9:20,] <- NA
+iAll <- as.matrix(ttrc[1:250,])
+iTop <- iAll; iTop[1:10,] <- NA
+iMid <- iAll; iMid[9:20,] <- NA
+
+hl  <- c('High','Low')
+hlc <- c('High','Low','Close')
+cl  <- 'Close'
 
 # Load output data
 load('unitTests/output.trend.rda')
@@ -22,42 +26,68 @@ load('unitTests/output.trend.rda')
 
 # ADX
 test.ADX <- function() {
-  checkEqualsNumeric( ADX(input$all[,c('High','Low','Close')]), output$allADX )
-  checkEqualsNumeric( ADX(input$top[,c('High','Low','Close')]), output$topADX )
-  #checkException( ADX(input$mid[,c('High','Low','Close')]) )
+  checkEqualsNumeric( ADX(iAll[,hlc]), output$allADX )
+  checkEqualsNumeric( ADX(iTop[,hlc]), output$topADX )
+  #checkException( ADX(iMid[,hlc]) )
 }
 
 # Aroon
-test.aroon <- function() {
-  checkEqualsNumeric( aroon(input$all[,c('High','Low')]), output$allAroon )
-  checkEqualsNumeric( aroon(input$top[,c('High','Low')]), output$topAroon )
-  #checkException( aroon(input$mid[,c('High','Low')]) )
+test.aroon.orig <- function() {
+  # non-xts
+  ia <- iAll[,hl]
+  it <- iTop[,hl]
+  im <- iMid[,hl]
+  rownames(ia) <- rownames(it) <- rownames(im) <- NULL
+  checkEqualsNumeric( aroon(ia), output$allAroon )
+  checkEqualsNumeric( aroon(it), output$topAroon )
+  #checkException( aroon(im) )
+}
+
+test.aroon.xts <- function() {
+  # xts
+  checkEqualsNumeric( aroon(iAll[,hl]), output$allAroon )
+  checkEqualsNumeric( aroon(iTop[,hl]), output$topAroon )
+  #checkException( aroon(iMid[,hl]) )
 }
 
 # Average True Range
-test.ATR <- function() {
-  checkEqualsNumeric( ATR(input$all[,c('High','Low','Close')]), output$allATR )
-  checkEqualsNumeric( ATR(input$top[,c('High','Low','Close')]), output$topATR )
-  #checkException( ATR(input$mid[,c('High','Low','Close')]) )
+test.ATR.orig <- function() {
+  # non-xts
+  ia <- iAll[,hlc]
+  it <- iTop[,hlc]
+  im <- iMid[,hlc]
+  rownames(ia) <- rownames(it) <- rownames(im) <- NULL
+  aATR <- ATR(ia);  rownames(aATR) <- rownames(iAll)
+  tATR <- ATR(it);  rownames(tATR) <- rownames(iTop)
+  checkEqualsNumeric( aATR, output$allATR )
+  checkEqualsNumeric( tATR, output$topATR )
+  #checkException( ATR(im) )
+}
+
+test.ATR.xts <- function() {
+  # xts
+  checkEqualsNumeric( ATR(iAll[,hlc]), output$allATR )
+  checkEqualsNumeric( ATR(iTop[,hlc]), output$topATR )
+  #checkException( ATR(iMid[,hlc]) )
 }
 
 # Commodity Channel Index
 test.CCI <- function() {
-  checkEqualsNumeric( CCI(input$all[,c('High','Low','Close')]), output$allCCI )
-  checkEqualsNumeric( CCI(input$top[,c('High','Low','Close')]), output$topCCI )
+  checkEqualsNumeric( CCI(iAll[,c('High','Low','Close')]), output$allCCI )
+  checkEqualsNumeric( CCI(iTop[,c('High','Low','Close')]), output$topCCI )
   #checkException( CCI(input$mid[,c('High','Low','Close')]) )
 }
 
 # Trend Detection Index
 test.TDI <- function() {
-  checkEqualsNumeric( TDI(input$all$Close), output$allTDI )
-  #checkEqualsNumeric( TDI(input$top$Close), output$topTDI )
-  #checkException( TDI(input$mid$Close) )
+  checkEqualsNumeric( TDI(iAll[,cl]), output$allTDI )
+  #checkEqualsNumeric( TDI(iTop[,cl]), output$topTDI )
+  #checkException( TDI(iMid[,cl]) )
 }
 
 # Vertical Horizontal Filter
 test.VHF <- function() {
-  checkEqualsNumeric( VHF(input$all$Close), output$allVHF )
-  #checkEqualsNumeric( VHF(input$top$Close), output$topVHF )
-  #checkException( VHF(input$mid$Close) )
+  checkEqualsNumeric( VHF(iAll[,cl]), output$allVHF )
+  #checkEqualsNumeric( VHF(iTop[,cl]), output$topVHF )
+  #checkException( VHF(iMid[,cl] )
 }

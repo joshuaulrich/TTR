3c91d2c2f6c2bef5adfa5db67640e5e632f4e9c0
Joshua M. Ulrich <josh.m.ulrich@gmail.com>
2011-05-30 03:34:18 +0000

    - fix adjRatios when close isn't supplied
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/ttr/pkg@112 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/adjRatios.R
+++ R/adjRatios.R
@@ -22,30 +22,32 @@ function(splits, dividends, close) {
 
   if( !missing(dividends) &&
        missing(close) )
-    stop('\'close\' must be specified to adjust dividends')
+    stop('"close" must be specified to adjust dividends')
        
   # Really need a better error message if as.xts fails... seriously
   if(missing(close) || all(is.na(close)) || NROW(close)==0) {
     close <- NA
   } else {
     close <- try.xts(close,
-      error=stop('\'as.xts(close)\' failed'))
+      error=stop('"as.xts(close)" failed'))
   }
   if(missing(splits) || all(is.na(splits)) || NROW(splits)==0) {
     splits <- NA
   } else {
     splits <- try.xts(splits,
-      error=stop('\'as.xts(splits)\' failed'))
+      error=stop('"as.xts(splits)" failed'))
   }
   if(missing(dividends) || all(is.na(dividends)) || NROW(dividends)==0) {
     dividends <- NA
   } else {
     dividends <- try.xts(dividends,
-      error=stop('\'as.xts(dividends)\' failed'))
+      error=stop('"as.xts(dividends)" failed'))
   }
 
   obj <- merge.xts(close,splits,dividends)
-  obj <- obj[!is.na(obj[,1]),]  # drop rows missing close prices
+  if(!isTRUE(is.na(close))) {
+    obj <- obj[!is.na(obj[,1]),]  # drop rows missing close prices
+  }
   adj <- .Call('adjRatios',obj[,2],obj[,3],obj[,1],PACKAGE="TTR")
   adj <- xts(cbind(adj[[1]],adj[[2]]),index(obj))
   colnames(adj) <- c('Split','Div')

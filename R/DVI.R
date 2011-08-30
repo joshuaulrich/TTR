142de2baebd9ca6d69a513b556d80095b27bb533
Joshua M. Ulrich <josh.m.ulrich@gmail.com>
2011-08-30 03:08:12 +0000

    - updated CHANGES, DESCRIPTION, and man/TTR.Rd - updated copyright to include 2011
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/ttr/pkg@116 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/DVI.R
+++ R/DVI.R
@@ -1,3 +1,22 @@
+#
+#   TTR: Technical Trading Rules
+#
+#   Copyright (C) 2007-2011  Joshua M. Ulrich
+#
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 DVI <- function(price, n=252, wts=c(0.8,0.2), smooth=3,
   magnitude=c(5,100,5), stretch=c(10,100,2)) {
 

b89cc2cd208db4f0083cea9dc9de27a0e3dc6430
bodanker <bodanker@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-08-10 00:40:54 +0000

    - Small fix to lags, thanks to Michael Weylandt

--- pkg/R/TTRtools.R
+++ pkg/R/TTRtools.R
@@ -20,14 +20,16 @@
 "lags" <-
 function(x, n=1) {
 
+  #.Deprecated(c("xts::lag.xts","quantmod::Lag"),"TTR")
+
   # Calculate lags of a series
 
   x <- as.matrix(x)
   if( is.null(colnames(x)) ) colnames(x) <- paste("V",1:NCOL(x),sep="")
 
-  out <- embed(x, lag+1)
-  if(lag==1)     lag.names <- 1      else
-  if(NCOL(x)==1) lag.names <- 1:lag  else  lag.names <- rep(1:lag,NCOL(x))
+  out <- embed(x, n+1)
+  if(n==1)       lag.names <- 1    else
+  if(NCOL(x)==1) lag.names <- 1:n  else  lag.names <- rep(1:n,NCOL(x))
 
   colnames(out) <- c( colnames(x), paste(colnames(x), sort(lag.names), sep=".") )
 

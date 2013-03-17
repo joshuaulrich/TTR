424b822d8a235ad69e7e3c88806876f254f62729
Joshua M. Ulrich <josh.m.ulrich@gmail.com>
2013-03-17 18:00:17 +0000

    - Update Copyright header date and license version (GPL3 -> GPL2)
    git-svn-id: svn+ssh://svn.r-forge.r-project.org/svnroot/ttr/pkg@165 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- R/pivots.R
+++ R/pivots.R
@@ -1,11 +1,11 @@
 #
 #   TTR: Technical Trading Rules
 #
-#   Copyright (C) 2007-2012  Joshua M. Ulrich
+#   Copyright (C) 2007-2013  Joshua M. Ulrich
 #
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
-#   the Free Software Foundation, either version 3 of the License, or
+#   the Free Software Foundation, either version 2 of the License, or
 #   (at your option) any later version.
 #
 #   This program is distributed in the hope that it will be useful,

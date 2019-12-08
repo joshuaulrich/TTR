774c4ec681739ddf1aa5170c8722b2be9a96ec6f
Joshua Ulrich <josh.m.ulrich@gmail.com>
2019-12-08 06:12:23 -0600

    Move declarations to header
    It's uncommon to have declarations in a source file.
    
    Also remove the `extern` from the function declarations. They are only
    needed for symbols (e.g. global symbols, imported functions).
    
    See #87.

--- src/ttr.h
+++ src/ttr.h
@@ -3,6 +3,25 @@
 
 #include <Rinternals.h>
 
-/* imported from xts */
+/* declare functions called via .Call() */
+SEXP adjRatios(SEXP, SEXP, SEXP);
+SEXP aroon_max(SEXP, SEXP);
+SEXP ema(SEXP, SEXP, SEXP, SEXP);
+SEXP evwma(SEXP, SEXP, SEXP);
+SEXP sar(SEXP, SEXP, SEXP, SEXP);
+SEXP ttr_rollPercentRank(SEXP, SEXP, SEXP, SEXP);
+SEXP ttr_zigzag(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+SEXP vma(SEXP, SEXP, SEXP);
+SEXP wilderSum(SEXP, SEXP);
+SEXP wma(SEXP, SEXP, SEXP);
+SEXP zlema(SEXP, SEXP, SEXP);
+SEXP runsum(SEXP, SEXP);
+SEXP runmin(SEXP, SEXP);
+SEXP runmax(SEXP, SEXP);
+SEXP runmedian(SEXP, SEXP, SEXP, SEXP);
+SEXP runmad(SEXP, SEXP, SEXP, SEXP, SEXP, SEXP);
+SEXP runcov(SEXP, SEXP, SEXP, SEXP, SEXP);
+
+/* declare xts imports */
 extern SEXP (*xts_na_check)(SEXP, SEXP);
 #endif

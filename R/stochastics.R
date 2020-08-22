cc203edf972aad61ef979e76c06b6c66863778d5
Joshua Ulrich <josh.m.ulrich@gmail.com>
2020-08-22 09:58:51 -0500

    Fix stoch() when fastK is Inf
    It may be possible for fastK to be Inf in very rare cases. I could only
    reproduce this if the Close is > High and High and Low are equal. But
    that is a data error. I fixed anyway because there may be cases I can't
    think of at the moment.
    
    Check for any non-finite values in fastK and replace them with 0.5.
    Make sure not to replace any NA, because leading NA need to propagate.
    
    Thanks to GitHub user cjuncosa for the report.
    
    Fixes #52.

--- R/stochastics.R
+++ R/stochastics.R
@@ -191,7 +191,7 @@ function(HLC, nFastK=14, nFastD=3, nSlowD=3, maType, bounded=TRUE, smooth=1, ...
     denMA <- do.call( maType[[3]][[1]], c( list(den), maType[[3]][-1] ) )
 
     fastK <- numMA / denMA
-    fastK[is.nan(fastK)] <- 0.5
+    fastK[!is.finite(fastK) & !is.na(fastK)] <- 0.5
     fastD <- do.call( maType[[1]][[1]], c( list(fastK), maType[[1]][-1] ) )
     slowD <- do.call( maType[[2]][[1]], c( list(fastD), maType[[2]][-1] ) )
   }
@@ -204,7 +204,7 @@ function(HLC, nFastK=14, nFastD=3, nSlowD=3, maType, bounded=TRUE, smooth=1, ...
     denMA <- do.call( maType, c( list(den), list(n=smooth) ) )
 
     fastK <- numMA / denMA
-    fastK[is.nan(fastK)] <- 0.5
+    fastK[!is.finite(fastK) & !is.na(fastK)] <- 0.5
     fastD <- do.call( maType, c( list(fastK), list(n=nFastD, ...) ) )
     slowD <- do.call( maType, c( list(fastD), list(n=nSlowD, ...) ) )
 
